b7c053c29b3bbe5f8af756ecd841898d3eb198e7
sellak mouad <mouaadsellak123@gmail.com>
2022-11-21 15:06:02 +0100

    new attemp to add Map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
 import OuMap from "./components/OuMap";
-
-function App(){
-  return(
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+function App() {
+  return (
     <div className="App" >
       <Navbar />
-      <OuMap  />
+      <OuMap />
     </div>
   )
 }
--- src/components/OuMap.js
+++ src/components/OuMap.js
@@ -18,6 +18,7 @@
 
 
 
+
 // import React from "react";
 // import GoogleMapReact from 'google-map-react';
 
@@ -49,38 +50,189 @@
 //   );
 // }
 
+
 // import React from 'react'
-// import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+// import { GoogleMap,useJsApiLoader, Marker } from '@react-google-maps/api';
 
 // const OuMap=()=>{
-//   const {isLoaded} = useLoadScript({
-//     googleMapsApiKey:"AIzaSyD6BbXbMR7-91GG9BGl6OjZAXSHCr1PATE"
+//   const {isLoaded} = useJsApiLoader({
+//     googleMapsApiKey:"AIzaSyD41VnECyFQMyRHzpps677Cux6uLhZvUhU"
 //   })
 //   if(!isLoaded) return <div>Loading...</div>
-//   return <Map />
+//   return(
+//     <GoogleMap zoom={15} center={{lat:44, lng:-88}} mapContainerStyle={{width:'100%', height:'100%'}}  ></GoogleMap>
+//   )
 // }
 
-// const Map=()=>{
-//   return <GoogleMap zoom={10} center={{lat:44, lng:-88}} ></GoogleMap>
-// }
 
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react'
+//import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from '@react-google-maps/api'
+import { useRef, useState } from 'react'
 
+const center = { lat: 48.8584, lng: 2.2945 }
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-const OuMap=()=>{
-  return(
-    <MapContainer center={[51.505, -0.09]} height={13} zoom={13} >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-    )
+function OuMap() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyD41VnECyFQMyRHzpps677Cux6uLhZvUhU",
+    libraries: ['places'],
+  })
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destiantionRef = useRef()
+
+  if (!isLoaded) {
+    return <SkeletonText />
+  }
+
+  async function calculateRoute() {
+    if (originRef.current.value === '' || destiantionRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destiantionRef.current.value = ''
+  }
+
+  return (
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={map => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+      <Box
+        p={4}
+        borderRadius='lg'
+        m={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='1'
+      >
+        <HStack spacing={2} justifyContent='space-between'>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input type='text' placeholder='Origin' ref={originRef} />
+            </Autocomplete>
+          </Box>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input
+                type='text'
+                placeholder='Destination'
+                ref={destiantionRef}
+              />
+            </Autocomplete>
+          </Box>
+
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+             // icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
+          <IconButton
+            aria-label='center back'
+          //  icon={<FaLocationArrow />}
+            isRound
+            onClick={() => {
+              map.panTo(center)
+              map.setZoom(15)
+            }}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  )
 }
+
 export default OuMap
+
+
+
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+// const OuMap=()=>{
+//   return(
+//     <MapContainer center={[13.505, 80.09]} height={13} zoom={13} >
+//     <TileLayer
+//       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//     />
+//     <Marker position={[51.505, -0.09]}>
+//       <Popup>
+//         A pretty CSS3 popup. <br /> Easily customizable.
+//       </Popup>
+//     </Marker>
+//   </MapContainer>
+//     )
+// }
