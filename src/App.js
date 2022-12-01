29b6bd21a323d888435ae737c0ca05079b302360
sellak mouad <mouaadsellak123@gmail.com>
2022-12-01 13:52:36 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from "./components/Navbar";
 import OuMap from "./components/OuMap";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 function App() {
   return (
     <div className="App" >
