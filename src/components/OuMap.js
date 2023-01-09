cdd6d61ce4fc621c1d2c7d1a5114adc61d16ae40
sellak mouad <mouaadsellak123@gmail.com>
2023-01-09 14:37:01 +0100

    update

--- src/components/OuMap.js
+++ src/components/OuMap.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader,Marker  } from '@react-google-maps/api';
+import React, {useState} from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { Button } from '@chakra-ui/react';
 
 const containerStyle = {
   width: '1000px',
   height: '600px',
-  margin:'50px',
+  margin: '50px',
 };
 
-const center = {
-  lat:47.93833,
-  lng:0.22167
-};
+
+const students = [
+   {
+    lat: 48.00454233910342,
+    lng: 0.21406909053153916
+  },
+   {
+    lat: 48.003393788665164,
+    lng: 0.20600100585967443
+  },
+  {
+    lat: 48.003393788665164,
+    lng: 0.20600100585967443
+  }
+]
+
 
 function OuMap() {
+  const [position,setPosition]= useState({
+    lat: 48.00448491218892,
+    lng: 0.22230883657940106
+  })
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyD41VnECyFQMyRHzpps677Cux6uLhZvUhU"
@@ -21,9 +38,8 @@ function OuMap() {
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
+    const bounds = new window.google.maps.LatLngBounds(position);
     map.fitBounds(bounds);
-
     setMap(map)
   }, [])
 
@@ -32,19 +48,29 @@ function OuMap() {
   }, [])
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={20}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        <Marker
-      onLoad={()=>console.log("marker")}
-      position={center}
-    />
-        <></>
-      </GoogleMap>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={position}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      zoom={1}
+    >
+      {/* {students.map( (s)=>
+         <Marker
+         onLoad={() => console.log("marker")}
+         position={s}
+         title="Marker"
+         />
+      )
+      }; */}
+      <Marker
+         onLoad={() => console.log("marker")}
+         position={position}
+         onClick={()=>console.log('CHNCJDBCDJ')}
+         title="Marker"
+         />
+      <></>
+    </GoogleMap>
   ) : <></>
 }
 
