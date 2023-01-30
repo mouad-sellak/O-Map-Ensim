6d87b074d6cf1ad22027ccc4e42566176bffe233
sellak mouad <mouaadsellak123@gmail.com>
2023-01-30 13:45:49 +0100

    add some updates

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,29 +1,26 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-  
+
 export default function Toggle() {
     const [enabled, setEnabled] = useState(false);
     const [toggleName, settoggleName] = useState("Map");
     return (
-        <div className="flex  ">
-            <label className="inline-flex relative items-center mr-5 cursor-pointer">
-                <input
-                    type="checkbox"
-                    className="sr-only peer  "
-                    checked={enabled}
-                    readOnly
-                />
-                <div
-                    onClick={() => {
-                        setEnabled(!enabled);
-                        toggleName === "Map" ?
-                        <Link to={{ pathname: "https://bachensim.github.io/OuMapEnsim/" }} target="_blank">AR</Link>  : 
-                         settoggleName("Map");
-                    }}
-                    className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-300"
-                ></div>
-                <span className="ml-2 text-sm font-bold  text-">{toggleName}</span>
-            </label> 
-        </div>
+            <div className="flex  ">
+                <label className="inline-flex relative items-center mr-5 cursor-pointer">
+                    <input
+                        type="checkbox"
+                        className="sr-only peer  "
+                        checked={enabled}
+                        readOnly
+                    />
+                    <div
+                        onClick={() => {
+                            setEnabled(!enabled);
+                            toggleName==="Map" ? settoggleName("AR") : settoggleName("Map");
+                        }}
+                        className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-300"
+                    ></div>
+                 <span className="ml-2 text-sm font-bold  text-white">{toggleName}</span> 
+                </label>
+            </div>
     );
 }
